1421e49ea8bd5a5c508ef399777de7ddc1bfb74f
naruto678 <arnabthakuria678@gmail.com>
2022-01-23 20:24:43 +0530

    adding more in readme and adding import lib capablities

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 #CSES problem set practice cli
 
-  Work in progress. 
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fcodeforces.com%2Fblog%2Fentry%2F87912&psig=AOvVaw2ydY4gUwVeX7Mdb_lnpoyH&ust=1643035884646000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCID3l-iPyPUCFQAAAAAdAAAAABAD">
+
+   The main objecttive of the module is to be able to run some file directly . 
+    usage would be as follows: 
+        The directory would be as follows: 
+            cses_problems: 
+                    -> Problem1: 
+                        input.txt
+                        problem_statement.txt
+                    ->Problem2: 
+                        input.txt
+                        problem_statment.txt
+
+        ./TaskRunner.py <problem-name>
+        ./TaskRunner.py <problem-name> --file <file-name to be used instead of the input.txt file present in the problem name directory >
+        
+    
+    Exception_handling : 
+        if input.txt is not found then prompt the user to put the input.txt file there ---> Future versions for this should try to login to the cses_problme set site 
+            and try parsing the html and getting the input to check it out . This is also would be used to document my journey of going through the cses problem set
+
+    Ideas: 
+    Also idea could be to run a single command and get the entire cses problem set and make the directory directly . 
+    Currently let us just support python and then we could move on to java / go-lang or rust. my new fav languages
+
+
+
+
 
--- TaskRunner.py
+++ TaskRunner.py
@@ -1,9 +1,15 @@
 import argparse
 from exceptions import * 
+import importlib
 
-def find_module(problem_name, input_file): 
-    return None, None
-
+def find_module(problem_name, input_file):
+    module, file_name=None, None
+    try: 
+        module=importlib.import_module(problem_name)
+    except ImportError: 
+        raise ProblemNotFound("{} not found ".format(problem_name))
+    print(module.__file__)
+    return module, "current_file_name"
 
 if __name__=='__main__':
     parser=argparse.ArgumentParser()
@@ -17,34 +23,4 @@ if __name__=='__main__':
         raise ProblemNotFound("Problem name not present in the current directory . Please correct the problem name or create a problem directory of the same name")
     if input_file is None:
         raise InputFileNotFound(" Input file not present in the specified problem direcotry . Please provide a path to the input file by specifying the file name inline by using the --file option . For more run the script ./run.sh with -h option ")
-
-        
-    '''
-    The main objecttive of the module is to be able to run soem file directly . 
-    usage would be as follows: 
-        The directory would be as follows: 
-            cses_problems: 
-                    -> Problem1: 
-                        input.txt
-                        problem_statement.txt
-                    ->Problem2: 
-                        input.txt
-                        problem_statment.txt
-
-        ./TaskRunner.py <problem-name>
-        ./TaskRunner.py <problem-name> --file <file-name to be used instead of the input.txt file present in the problem name directory >
-        
-    
-    Exception_handling : 
-        if input.txt is not found then prompt the user to put the input.txt file there ---> Future versions for this should try to login to the cses_problme set site 
-            and try parsing the html and getting the input to check it out . This is also would be used to document my journey of going through the cses problem set
-
-    Ideas: 
-    Also idea could be to run a single command and get the entire cses problem set and make the directory directly . 
-    Currently let us just support python and then we could move on to java / go-lang or rust. my new fav languages
-
-
-
-    '''
-    print("Please visit later. Currently at work ")
 
